8756779c2c92ca8d8ef870624e32f30242d3204b
mtyton <46006692+mtyton@users.noreply.github.com>
2019-04-23 14:22:01 +0200

    Update clent.py

--- client/clent.py
+++ client/clent.py
@@ -10,7 +10,7 @@ def receive():
         try:
             msg = client_socket.recv(BUFSIZE).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError("Cleint propably left :("):
+        except OSError("Client propably left :("):
             break
 
 
@@ -47,21 +47,22 @@ if __name__ == "__main__":
         port, host = set_defaults()
     port = int(port)
     addr = (host, port)
-    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket = socket(AF_INET, SOCK_STREAM) # creating socket for a client
     client_socket.connect(addr)
 
-    top = tkinter.Tk()
+    top = tkinter.Tk() # creating our window object
     top.title("Chatter")
     message_frame = tkinter.Frame(top)
     msg = tkinter.StringVar()
     msg.set("Type your messages here.")
     scrollbar = tkinter.Scrollbar(message_frame)
     message_frame.pack()
+    # there we place our received messages
     msg_list = tkinter.Listbox(message_frame, height=15, width=60, yscrollcommand=scrollbar.set)
     scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
     msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 
-
+    # entry field part
     entry_field = tkinter.Entry(top, textvariable=msg)
     entry_field.bind("<Return>", send)
     entry_field.pack()
@@ -69,6 +70,7 @@ if __name__ == "__main__":
     send_button.pack()
     top.protocol("WM_DELETE_WINDOW", on_closing)
 
+    #simply starts a thread which handles receiving messages
     receive_thread = Thread(target=receive)
     receive_thread.start()
     tkinter.mainloop()
